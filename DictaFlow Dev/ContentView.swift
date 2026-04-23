6728cd8e9107bbb34a8d3a3f70881673f7661781
Rifky Manuel Satyana <rmscoaldev@gmail.com>
2026-04-23 20:01:44 +0700

    Initial Commit

---
+++ DictaFlow Dev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DictaFlow Dev
+//
+//  Created by Rifky Manuel Satyana on 23/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DictaFlow Dev/DictaFlow_DevApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictaFlow_DevApp.swift
+//  DictaFlow Dev
+//
+//  Created by Rifky Manuel Satyana on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct DictaFlow_DevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
